b726b907920c816e11aa505949c96341f67853b3
vlasik <vlasiigolovin@gmail.com>
2024-02-29 16:34:28 +0100

    upd

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
 
+from db_scripts import PlanesDB
+
 app = Flask(__name__)
+db = PlanesDB("planes.db")
 
 @app.route("/")
 def main_page():
     title = "PlanePedia - your source about planes"
-    return render_template("index.html", title = title)
+    planes = db.get_all_planes()
+
+    return render_template("index.html", title = title, planes = planes)
+
+
+
+    
 
---
+++ db_scripts.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+class PlanesDB:
+    def __init__(self, dbname):
+        self.name = dbname
+        self.conn = None
+        self.cursor = None
+
+    def open(self):
+        self.conn = sqlite3.connect(self.name)
+        self.conn.row_factory = sqlite3.Row
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
+    def get_all_planes(self):
+        self.open()
+        self.cursor.execute('''SELECT * FROM planes ''')
+        data = self.cursor.fetchall()
+        self.close()
+        data = [dict(row) for row in data]
+        return data
+    
