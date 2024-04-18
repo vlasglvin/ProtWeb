e70206a34819b5dfcc881b69be9e875f9dcc46cb
vlasik <vlasiigolovin@gmail.com>
2024-04-18 17:34:01 +0200

    upd

---
+++ admin.py
@@ -0,0 +1,66 @@
+from flask import Flask, Blueprint, render_template,  request, redirect, url_for, flash
+from db_scripts import PlanesDB
+from flask_login import UserMixin, current_user, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from settings import *
+from app import login_manager, db
+
+admin = Blueprint('admin', __name__,
+                        template_folder='admin_templates')
+
+
+@admin.route("/admin", methods=["GET", "POST"])
+@login_required
+def admin_page():
+    if current_user.role != "admin":
+                return redirect(url_for('main_page'))
+    return render_template("admin_page.html", title = "Administartor")
+
+@admin.route("/admin/new_plane", methods=["GET", "POST"])
+@login_required
+def add_plane_page():
+    if current_user.role != "admin":
+                return redirect(url_for('main_page'))
+    
+    plane_types = db.get_all_categories()
+    if request.method == 'POST':
+        name = request.form['name']
+        category_id = request.form['category']
+        image = request.files['image'] 
+        country = request.form['country']
+        quantity = request.form['quantity']
+        prodused_start = request.form['prodused_start']
+        prodused_end = request.form['prodused_end']
+        cost = request.form['cost']
+        wing_shape = request.form['wing_shape']
+        specifications = request.form['specifications']
+        description = request.form['description']
+        history = request.form['history']
+
+        is_valid = True
+        for field in [name, category_id, country, quantity, prodused_start, cost, wing_shape, description, history]:
+            if field.strip() == "":
+                is_valid = False
+                break
+        
+        try:
+            if is_valid:
+                if request.form.get('present_days') == 'present_days':
+                    prodused_end = "present-day"
+                if image:
+                    image.save(IMG_PATH + image.filename )
+                db.create_plane(name, int(category_id), image.filename, country, quantity,
+                                prodused_start, prodused_end, cost, wing_shape, specifications, description, history)
+
+                flash("Plane added succesfully.", category="alert-primary")
+            else:
+                flash("Please fill in all fields", category="alert-danger")
+        except:
+            flash("Error. Try again!", category="alert-danger")
+
+        
+    
+    return render_template("add_plane.html", title = "Add new plane", plane_types = plane_types)
+
+
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,  request, redirect, url_for, flash
+from flask import Flask, Blueprint, render_template,  request, redirect, url_for, flash
 from dotenv import load_dotenv
 from datetime import timedelta
 from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
@@ -7,11 +7,12 @@ from db_scripts import PlanesDB
 import os
 load_dotenv()
 
-PATH = os.path.dirname(__file__) + os.sep
-IMG_PATH = PATH + "static" + os.sep + "img" + os.sep
+from settings import *
+
 
 app = Flask(__name__)
 db = PlanesDB("planes.db")
+
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SESSION_REMEMBER_ME_DURATION'] = timedelta(days=31)
@@ -20,20 +21,10 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = "alert-warning"
 
-class User(UserMixin):
-    def __init__(self, user_data):
-        super().__init__()
-        self.id = str(user_data['id'])
-        self.name = user_data['name']
-        self.username = user_data['username']
-        self.role = user_data['role']
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    user_data = db.get_user(user_id)
-    return User(user_data)
-
+from admin import admin
+from accounts import accounts
+app.register_blueprint(accounts)
+app.register_blueprint(admin)
 
 @app.route("/")
 def main_page():
@@ -96,127 +87,3 @@ def articles():
                             articles = articles,
                             plane_types=plane_types)
 
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main_page'))
-    
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if 'remember_me' in request.form:
-            remember = True
-        else:
-            remember = False
-        user_db = db.check_login_data(username)
-        if user_db and check_password_hash(user_db['password'], password):
-            
-            user = User(user_db)
-            login_user(user, remember=remember)
-            if user.role == "admin":
-                return redirect(url_for('admin_page'))
-
-            return redirect(url_for('main_page'))
-        
-        else:
-            flash("Incorrect username or password", category="alert-warning")
-
-    return render_template("login.html", title = "Login")
-
-
-@app.route("/admin", methods=["GET", "POST"])
-@login_required
-def admin_page():
-    if current_user.role != "admin":
-                return redirect(url_for('main_page'))
-    return render_template("admin_page.html", title = "Administartor")
-
-@app.route("/admin/new_plane", methods=["GET", "POST"])
-@login_required
-def add_plane_page():
-    if current_user.role != "admin":
-                return redirect(url_for('main_page'))
-    
-    plane_types = db.get_all_categories()
-    if request.method == 'POST':
-        name = request.form['name']
-        category_id = request.form['category']
-        image = request.files['image'] 
-        country = request.form['country']
-        quantity = request.form['quantity']
-        prodused_start = request.form['prodused_start']
-        prodused_end = request.form['prodused_end']
-        cost = request.form['cost']
-        wing_shape = request.form['wing_shape']
-        specifications = request.form['specifications']
-        description = request.form['description']
-        history = request.form['history']
-
-        is_valid = True
-        for field in [name, category_id, country, quantity, prodused_start, cost, wing_shape, description, history]:
-            if field.strip() == "":
-                is_valid = False
-                break
-        
-        try:
-            if is_valid:
-                if request.form.get('present_days') == 'present_days':
-                    prodused_end = "present-day"
-                if image:
-                    image.save(IMG_PATH + image.filename )
-                db.create_plane(name, int(category_id), image.filename, country, quantity,
-                                prodused_start, prodused_end, cost, wing_shape, specifications, description, history)
-
-                flash("Plane added succesfully.", category="alert-primary")
-            else:
-                flash("Please fill in all fields", category="alert-danger")
-        except:
-            flash("Error. Try again!", category="alert-danger")
-
-        
-    
-    return render_template("add_plane.html", title = "Add new plane", plane_types = plane_types)
-
-
-@app.route("/admin/logout")
-def logout():
-    logout_user()
-    return redirect(url_for('main_page'))
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('main_page'))
-    
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        username = request.form['username']
-        password = request.form['password']
-        password_repeat = request.form['password_repeat']
-        
-        is_valid =  name.strip() != "" and email.strip() != "" and username.strip() != "" and password.strip() != "" and password_repeat.strip() != ""
-        
-        if is_valid:    
-            if db.is_username_exist(username):
-                flash("this username is already taken", category="alert-warning")
-
-            elif db.is_email_exist(email):
-                flash("this email is already taken", category="alert-warning")
-            elif password!= password_repeat:
-                flash("Passwords must match", category="alert-warning")
-            elif len(password) < 8:
-                flash("Password has to be longer than 8 characters", category="alert-warning")
-               
-            else:
-                password_hash = generate_password_hash(password)
-                db.create_user(name,username, email, password_hash)
-                flash("User created", category="alert-primary")
-                return redirect(url_for('login'))
-
-        else:
-            flash("Please fill in all fields", category="alert-warning")
-
-
-    return render_template("register.html", title = "Create new account")
